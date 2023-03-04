8e8132cc17e49d70fc7b920f0168d2dfcec3bc7a
pointlesspun <anotherpointlesspun@gmail.com>
2023-03-04 15:56:29 +0100

    Finish Mermaid tooling

--- c_sharp/gg.ast.tests/util/MermaidTest.cs
+++ c_sharp/gg.ast.tests/util/MermaidTest.cs
@@ -4,27 +4,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using gg.ast.interpreter;
 using gg.ast.util;
+using System.IO;
 
 namespace gg.ast.tests.util
 {
     [TestClass]
     public class MermaidTest
     {
+        /// <summary>
+        /// Integration test to see if the mermaid export works (somewhat) using 
+        /// depthfirst traversal.
+        /// </summary>
         [TestMethod]
-        public void depthFirstTest()
+        public void DepthFirstTest()
         {
             // use https://mermaid-js.github.io/mermaid-live-editor to visualize
             var mermaid = new MermaidOutput()
             {
-                AllowDuplicates = true,
+                AllowDuplicateBranches = false,
                 ShowReferenceRules = true,
             };
             var mermaidRules = new ParserFactory().ParseFileRules("./util/mermaid.spec");
             var jsonRules = new ParserFactory().ParseFileRules("./util/json.spec");
-            var mermaidChart = mermaid.ToMermaidChart(mermaidRules["mermaidChart"]);
 
-            mermaid.ToChart(mermaidRules["mermaidChart"]);
-            mermaid.ToChart(jsonRules["document"]);
+            mermaid.ToChartFile(mermaidRules["mermaidChart"]);
+            mermaid.ToChartFile(jsonRules["document"]);
+
+            Assert.IsTrue(File.Exists("./mermaidChart.mmc"));
+            Assert.IsTrue(File.Exists("./document.mmc"));
+            Assert.IsTrue(new FileInfo("./mermaidChart.mmc").Length > 0);
+            Assert.IsTrue(new FileInfo("./document.mmc").Length > 0);
         }
     }
 }
--- c_sharp/gg.ast/util/MermaidOutput.cs
+++ c_sharp/gg.ast/util/MermaidOutput.cs
@@ -15,15 +15,16 @@ namespace gg.ast.util
     /// </summary>
     public class MermaidOutput
     {
-        /// <summary>
-        /// Export the nodes in DepthFirst or BreadthFirst order (which at the moment
-        /// Mermaid gleefully ignores for the most part).
-        /// </summary>
-        public enum Order
-        {
-            DepthFirst,
-            BreadthFirst
-        }
+        // class names used in the rules
+        private static readonly string ReferenceClassName = "referenceRule";
+        private static readonly string LiteralClassName = "literalRule";
+        private static readonly string CharClassName = "charRule";
+
+        // styling for each of the rules
+        private static readonly string NodeClasses = 
+            $"classDef {ReferenceClassName} fill:#EEEEEF,stroke:none\n"
+            + $"classDef {LiteralClassName} fill:#EEEEAA,stoke:AAAA99\n"
+            + $"classDef {CharClassName} fill:#EEEEAA,stroke:none\n";
 
         /// <summary>
         /// Indentation of the diagram at the first node
@@ -35,11 +36,22 @@ namespace gg.ast.util
         /// </summary>
         public int IndentationIncrement { get; set; } = 2;
 
+        /// <summary>
+        /// If set to true all rules with NodeVisibility set to Transitive or Hidden will
+        /// be removed from the diagram
+        /// </summary>
         public bool CullNotVisibleNodes { get; set; } = false;
 
-        public bool AllowDuplicates { get; set; } = false;
+        /// <summary>
+        /// If set to true nodes that have been visited in other branches before will be shown
+        /// as a full branch instead of a reference 
+        /// </summary>
+        public bool AllowDuplicateBranches { get; set; } = false;
 
-        public bool ShowReferenceRules { get; set; } = false;
+        /// <summary>
+        /// IF set to true the diagram will show reference rules
+        /// </summary>
+        public bool ShowReferenceRules { get; set; } = true;
 
         /// <summary>
         /// Writes a mermaid chart using the given rule to a file.
@@ -48,17 +60,24 @@ namespace gg.ast.util
         /// <param name="file">Optional filename, if no filename is given the tag of the rule will be used</param>
         /// <param name="chartType">Mermaid chart type (defaults to flowchart top-down)</param>
         /// <param name="order">Order in which the nodes have to be written to the file depth first or breadth first</param>
-        public void ToMDFile(IRule rule, string file = null, string chartType= "flowchart TD", Order order = Order.DepthFirst)
+        public void ToMDFile(IRule rule, string file = null, string chartType= "flowchart TD")
         {
             var fileName = file ?? rule.Tag + ".md";
-            var text = $"```mermaid\n{chartType}\n{ToMermaidChart(rule, order)}\n```";
+            var text = $"```mermaid\n{chartType}\n{ToMermaidChart(rule)}\n```";
             File.WriteAllText(fileName, text);
         }
 
-        public void ToChart(IRule rule, string file = null, string chartType = "flowchart TD", Order order = Order.DepthFirst)
+        /// <summary>
+        /// Saves the chart to a .mmc file
+        /// </summary>
+        /// <param name="rule"></param>
+        /// <param name="file"></param>
+        /// <param name="chartType"></param>
+        /// <param name="order"></param>
+        public void ToChartFile(IRule rule, string file = null, string chartType = "flowchart TD")
         {
-            var fileName = file ?? rule.Tag + ".md";
-            File.WriteAllText(fileName, chartType + "\n" + ToMermaidChart(rule, order));
+            var fileName = file ?? rule.Tag + ".mmc";
+            File.WriteAllText(fileName, chartType + "\n" + ToMermaidChart(rule));
         }
 
         /// <summary>
@@ -67,44 +86,50 @@ namespace gg.ast.util
         /// <param name="rule">The rule for which the diagram has to be build</param>
         /// <param name="order">Order in which the nodes have to be written to the file depth first or breadth first</param>
         /// <returns>a string containing the chart</returns>
-        public string ToMermaidChart(IRule rule, Order order = Order.DepthFirst)
+        public string ToMermaidChart(IRule rule)
         {
-            return order switch
-            {
-                Order.DepthFirst => ToStringDepthFirst(rule, indentation: StartIndentation),
-                Order.BreadthFirst => ToStringBreadthFirst(rule),
-                _ => "unknown order"
-            };
-        }
+            var builder = new StringBuilder();
 
+            BuildGraph(rule, builder);
+
+            return builder.ToString() + "\n" + NodeClasses;
+        }
+         
         /// <summary>
         /// Writes the rule tree to the string builder in a depth first order.
         /// </summary>
-        /// <param name="rule"></param>
-        /// <param name="parentId"></param>
-        /// <param name="builder"></param>
-        /// <param name="visitedRules"></param>
-        /// <param name="indentation"></param>
+        /// <param name="rule">The current rule to map to a diagram</param>
+        /// <param name="parentId">The full id of a parent consisting of a string forming of Rule.Ids forming the path 
+        /// from the parent to the root</param>
+        /// <param name="parentHash">The hashcode of the parent</param>
+        /// <param name="builder">String builder holding the graph</param>
+        /// <param name="visitedRules">Rules which have been mapped to a graph before (keeping track of loops)</param>
+        /// <param name="indentation">Current </param>
         /// <returns></returns>
-        private string ToStringDepthFirst(
-            IRule rule, 
+        private void BuildGraph(
+            IRule rule,
+            StringBuilder builder,
             string parentId = "", 
-            StringBuilder builder = null, 
+            string parentHash = "",
             Dictionary<IRule, string> visitedRules = null,  
             int indentation = 0)
         {
-            builder ??= new StringBuilder();
             visitedRules ??= new Dictionary<IRule, string>();
 
             var includeNode = (ShowReferenceRules || !(rule is ReferenceRule)) 
                             && (!CullNotVisibleNodes || rule.Visibility == NodeVisiblity.Visible);
             var ruleIdString = parentId + "_" + rule.Id;
-            
+
+            // we need a custom hash of the current string to get a hash to specific node at 
+            // a specific location in the tree which this does. If not (and we're using for instance
+            // the rule id) the graph gets too clever and starts reuse the same nodes
+            var ruleHash     = ruleIdString.GetHashCode().ToString("x4");
+
             // if this node has been added to the graph before connect the parent to the other
             // instance
             if (visitedRules.TryGetValue(rule, out string value))
             {
-                builder.AppendLine($"{parentId} --> {value}".AddPrefix(indentation + IndentationIncrement));
+                builder.AppendLine($"{parentHash} -.-> {value}".AddPrefix(indentation + IndentationIncrement));
             }
             else
             {
@@ -113,14 +138,17 @@ namespace gg.ast.util
                     // connect the parent to this node (if any)
                     if (!string.IsNullOrEmpty(parentId))
                     {
-                        builder.AppendLine($"{parentId} --> {ruleIdString}".AddPrefix(indentation));
+                        builder.AppendLine($"{parentHash} --> {ruleHash}".AddPrefix(indentation));
                     }
 
                     // specify the node's text
-                    var ruleText = $"{ruleIdString}(\"{CreateRuleText(rule)}\")".AddPrefix(indentation);
+                    var ruleText = $"{ruleHash}{CreateRuleMarkup(rule)}".AddPrefix(indentation);
                     builder.AppendLine(ruleText);
-                    visitedRules[rule] = ruleIdString;
+                    visitedRules[rule] = ruleHash;
                 }
+
+                var subruleParent = includeNode ? ruleIdString : parentId;
+                var subruleParentHash = includeNode ? ruleHash : parentHash;
 
                 // add subrules of a IRulegroup
                 if (rule is IRuleGroup group && group.Subrules != null && group.Subrules.Length > 0)
@@ -128,26 +156,25 @@ namespace gg.ast.util
                     for (var i = 0; i < group.Subrules.Length; i++ )
                     {
                         var subrule = group.Subrules[i];
-                        var subruleVisitedNodes = AllowDuplicates ? new Dictionary<IRule, string>(visitedRules) : visitedRules;
-                        var subruleParent = includeNode ? ruleIdString : parentId;
-                        
-                        ToStringDepthFirst(subrule, subruleParent, builder, subruleVisitedNodes, indentation + IndentationIncrement);
+                        var subruleVisitedNodes = AllowDuplicateBranches ? new Dictionary<IRule, string>(visitedRules) : visitedRules;
+        
+                        BuildGraph(subrule, builder, subruleParent, subruleParentHash, subruleVisitedNodes, indentation + IndentationIncrement);
                     }
                 }
                 // add subrules of a IMetaRule
                 else if (rule is IMetaRule metaRule && metaRule.Subrule != null)
                 {
-                    var subruleVisitedNodes = AllowDuplicates ? new Dictionary<IRule, string>(visitedRules) : visitedRules;
-                    var subruleParent = includeNode ? ruleIdString : parentId;
+                    var subruleVisitedNodes = AllowDuplicateBranches ? new Dictionary<IRule, string>(visitedRules) : visitedRules;
 
-                    ToStringDepthFirst(metaRule.Subrule, subruleParent, builder, subruleVisitedNodes, indentation + IndentationIncrement);
+                    BuildGraph(metaRule.Subrule, builder, subruleParent, subruleParentHash, subruleVisitedNodes, indentation + IndentationIncrement);
                 }
             }
+        }        
 
-            return builder.ToString();
-        }
-
-        private string ToStringBreadthFirst(IRule root) 
+        //
+        // Breadth first declarations don't really work in mermaid at the moment... 
+        // revisit at a later stage.
+        /*private string ToStringBreadthFirst(IRule root) 
         {
             var visitedRules = new Dictionary<IRule, string>();
             var openList = new List<(IRule rule, string parentId, int indentation, int index)>() { (root, "", 0, 0) };
@@ -172,7 +199,7 @@ namespace gg.ast.util
                         builder.AppendLine($"rule_{parentId} --> {ruleName}".AddPrefix(indentation));
                     }
 
-                    var ruleText = $"{ruleName}(\"{CreateRuleText(rule)}\")".AddPrefix(indentation);
+                    var ruleText = $"{ruleName}{CreateRuleMarkup(rule)}".AddPrefix(indentation);
                     builder.AppendLine(ruleText);
                     visitedRules[rule] = id;
 
@@ -191,14 +218,25 @@ namespace gg.ast.util
             }
 
             return builder.ToString();
-        }
-
-        private static string CreateRuleName(string parentId, int child)
+        }*/
+        
+        /// <summary>
+        /// Determine shape and styling of the node
+        /// </summary>
+        /// <param name="rule"></param>
+        /// <returns></returns>
+        private static string CreateRuleMarkup(IRule rule)
         {
-            return $"rule_{parentId}_{child}";
+            return rule switch
+            {
+                ReferenceRule _ => $"[/\"{CreateRuleLabel(rule)}\"/]:::{ReferenceClassName}",
+                LiteralRule _ => $"[\"{CreateRuleLabel(rule)}\"]:::{LiteralClassName}",
+                CharRule _ => $"[\"{CreateRuleLabel(rule)}\"]:::{CharClassName}",
+                _ => $"[\"{CreateRuleLabel(rule)}\"]"
+            };
         }
 
-        private static string CreateRuleText(IRule rule)
+        private static string CreateRuleLabel(IRule rule)
         {
             var tag = rule.Tag.IndexOf("(unnamed)") < 0 ? rule.Tag : "";
 
@@ -209,7 +247,7 @@ namespace gg.ast.util
                 SequenceRule _ => $"{tag}(sequence)",
                 RepeatRule _ => rule.Tag,
                 NotRule _ => "not",
-                CharRule chr => CharRuleString(chr),
+                CharRule chr => CharRuleLabel(chr),
                 CriticalRule _ => "critical",
                 ReferenceRule reference => $"{reference.Reference}(ref)",
                 _ => "unknown type"
@@ -228,7 +266,12 @@ namespace gg.ast.util
             return typeText;
         }
 
-        private static string CharRuleString(CharRule rule)
+        /// <summary>
+        /// Specific mappings for a CharRule
+        /// </summary>
+        /// <param name="rule"></param>
+        /// <returns></returns>
+        private static string CharRuleLabel(CharRule rule)
         {
             string match = rule.MatchCharacters switch
             {
@@ -242,10 +285,15 @@ namespace gg.ast.util
 
             return rule.Characters == null 
                 ? match 
-                : match + " '" + Escape(rule.Characters) + "'";
+                : match + " '" + UnEscape(rule.Characters) + "'";
         }
 
-        private static string Escape(string characters)
+        /// <summary>
+        /// Replace invisible characters with visible characters
+        /// </summary>
+        /// <param name="characters"></param>
+        /// <returns></returns>
+        private static string UnEscape(string characters)
         {
             var builder = new StringBuilder();
 
