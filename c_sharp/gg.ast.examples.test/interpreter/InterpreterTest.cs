2030d3de8dae4a9fe84d09db9913be89a0f9d1fd
pointlesspun <anotherpointlesspun@gmail.com>
2023-03-09 09:44:24 +0100

    json files can now be parsed

--- c_sharp/gg.ast.examples.test/interpreter/InterpreterTest.cs
+++ c_sharp/gg.ast.examples.test/interpreter/InterpreterTest.cs
@@ -372,8 +372,8 @@ namespace gg.ast.examples.test.interpreter
             var donutValue = factory.ValueMap.Map<string>(donuts, donutNode);
             Assert.IsTrue(donutValue == "donut");
 
-            donutsResult.Nodes[0].ToString((s) => Debug.Write(s), donuts);
-            scienceResult.Nodes[0].ToString((s) => Debug.Write(s), science);
+            // donutsResult.Nodes[0].ToString((s) => Debug.Write(s), donuts);
+            // scienceResult.Nodes[0].ToString((s) => Debug.Write(s), science);
         }
 
         private (IRule interpreter, ParserFactory factory, Dictionary<string, IRule> rules) InitializeTest(
